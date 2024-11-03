b6350a773af96dc2a77b82dd8574f253cdf5a1e3
leena aden <adenleena@gmail.com>
2024-11-03 18:51:53 +0300

    openeded and closed text using slectirs inside the element

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question")
+questions.forEach(function(item){
+  const btn = item.querySelector(".question-btn")
+  btn.addEventListener("click",function(){
 
+    questions.forEach(function(question){
+      if(question != item){
+        question.classList.remove("show-text")
+      }
+    })
+    item.classList.toggle("show-text")
+  })
+
+})
 
 
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn")
+// const btns = document.querySelectorAll(".question-btn")
 
-btns.forEach(function(btn){
-  btn.addEventListener("click",function(e){
-    const question = e.currentTarget.parentElement.parentElement
-    question.classList.toggle("show-text")
-  })
-})
+// btns.forEach(function(btn){
+//   btn.addEventListener("click",function(e){
+//     const question = e.currentTarget.parentElement.parentElement
+//     question.classList.toggle("show-text")
+//   })
+// })
